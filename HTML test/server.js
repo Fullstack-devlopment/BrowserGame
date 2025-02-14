f78b7ab4ea6d1445b7473d7a8b81fcf92baf2f72
Balder J <baldertjacobsen@gmail.com>
2025-02-14 10:17:31 +0100

    change port number

--- HTML test/server.js
+++ HTML test/server.js
@@ -15,7 +15,7 @@ const server = http.createServer((req, res) => {
     });
 });
 
-const PORT = 3000;
+const PORT = 10241;
 server.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
