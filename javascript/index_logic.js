c8f366e6a680dfa4cea20a7c2854ff2690fbe8f0
karlpedro <162471444+karlpedro@users.noreply.github.com>
2025-02-24 19:14:54 +0100

    alerts and moved joingame logic

--- javascript/index_logic.js
+++ javascript/index_logic.js
@@ -7,8 +7,8 @@ function createGame() {
     })
     .then(response => response.json())
     .then(data => {
-        if (data.message === 'Game created successfully: ', data.gameId) {
-            console.log('Game created with ID:', data.gameId);
+        if (data.message === 'Game created successfully: ') {
+            alert('Game created successfully!: ', data.gameId);
         } else {
             alert('Failed to create game: ' + data.message);
         }
@@ -17,4 +17,32 @@ function createGame() {
         console.error('Error:', error);
         alert('An error occurred while creating the game.');
     });
+}
+
+function joinGame() {
+    let username = document.getElementById('usernameInput').value.trim();
+    if (username !== "") {
+        // Send a POST request to the backend to add the player
+        fetch('http://130.225.170.52:10241/addPlayer', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username: username })
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.message === 'Player added successfully') {
+                alert('Player added:', data.player.id, data.player.name);
+            } else {
+                alert('Failed to join game: ' + data.message);
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('An error occurred while joining the game.');
+        });
+    } else {
+        alert('Du skal indtaste et brugernavn');
+    }
 }
