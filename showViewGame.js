733b5e020e191cbf77e70e19ca0e007f49423f43
Viktor Steffensen <s214707@dtu.dk>
2025-03-03 15:42:29 +0100

    setup finalResult page

--- showViewGame.js
+++ showViewGame.js
@@ -3,6 +3,7 @@ function showView(viewId) {
     document.getElementById('writeAnswer').style.display = 'none';
     document.getElementById('voteAnswer').style.display = 'none';
     document.getElementById('bestAnswer').style.display = 'none';
+    document.getElementById('resultView').style.display = 'none';
 
 
     // show the selected view
