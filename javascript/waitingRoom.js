0f52ca778a37f3b86c437c60ad3004f9845d1bf4
Balder J <baldertjacobsen@gmail.com>
2025-03-10 13:33:58 +0100

    Organize code

---
+++ javascript/waitingRoom.js
@@ -0,0 +1,13 @@
+// Initialize the socket connection
+const socket = io('https://130.225.170.52:10242'); // Use your actual server address and port
+
+socket.on('playerJoined', (data) => {
+    // Check if the event is for the current game
+    const currentGameId = document.getElementById('gameIdDisplay').textContent;
+    if (data.gameId === currentGameId) {
+        const playerList = document.getElementById('playerList');
+        const li = document.createElement('li');
+        li.textContent = data.playername;
+        playerList.appendChild(li);
+    }
+});
