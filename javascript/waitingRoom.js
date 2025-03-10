67d4d17d1381b20b442d5dbf348aa18da2105036
Balder J <baldertjacobsen@gmail.com>
2025-03-10 15:11:31 +0100

    Test if we get what we want

--- javascript/waitingRoom.js
+++ javascript/waitingRoom.js
@@ -7,7 +7,7 @@ socket.on('playerJoined', (data) => {
     if (data.gameId === currentGameId) {
         const playerList = document.getElementById('playerList');
         const li = document.createElement('li');
-        li.textContent = data.playername;
+        li.textContent = typeof data.playername === 'object' ? data.playername.name : data.playername;
         playerList.appendChild(li);
     }
 });
