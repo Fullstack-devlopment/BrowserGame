2381b50384de0bb531f63361020d9df813b9f760
Viktor Steffensen <s214707@dtu.dk>
2025-03-10 13:26:40 +0100

    changed order of scripts

--- javascript/updatePlayerList.js
+++ javascript/updatePlayerList.js
@@ -1,24 +1,24 @@
 // Function to update the player list in the waiting room view
 async function updatePlayerList() {
-try {
-    const response = await fetch('/users');
-    if(!response) {
-        throw new Error('Kunne ikke finde spillere');
+    try {
+        const response = await fetch('/users');
+        if(!response) {
+            throw new Error('Kunne ikke finde spillere');
+        }
+        const players = await response.json();
+
+        const playerListContainer = document.getElementById("listOfPlayers");
+        playerListContainer.innerHTML = '';  // Clear existing list
+
+        const colors = ["one", "two", "three", "four", "five", "six"];
+        // Loop through the players and display them
+        players.forEach((player, index) => {
+            const playerElement = document.createElement("p");
+            playerElement.textContent = player;
+            playerElement.classList.add(colors[index % colors.length]);
+            playerListContainer.appendChild(playerElement);
+        });
+    } catch(error) {
+        console.error('Fejl ved at finde spillere', error);
     }
-    const players = await response.json();
-
-    const playerListContainer = document.getElementById("listOfPlayers");
-    playerListContainer.innerHTML = '';  // Clear existing list
-
-    const colors = ["one", "two", "three", "four", "five", "six"];
-    // Loop through the players and display them
-    players.forEach((player, index) => {
-        const playerElement = document.createElement("p");
-        playerElement.textContent = player;
-        playerElement.classList.add(colors[index % colors.length]);
-        playerListContainer.appendChild(playerElement);
-    });
-} catch(error) {
-    console.error('Fejl ved at finde spillere', error);
-}
 }
