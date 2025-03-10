516a91c51f18eff4c1f949a03f219c0461992b2b
Balder J <baldertjacobsen@gmail.com>
2025-03-10 15:29:16 +0100

    Update updatePlayerList.js

--- javascript/updatePlayerList.js
+++ javascript/updatePlayerList.js
@@ -14,7 +14,7 @@ async function updatePlayerList() {
         // Loop through the players and display them
         players.forEach((player, index) => {
             const playerElement = document.createElement("p");
-            playerElement.textContent = player;
+            playerElement.textContent = player.playername;
             playerElement.classList.add(colors[index % colors.length]);
             playerListContainer.appendChild(playerElement);
         });
