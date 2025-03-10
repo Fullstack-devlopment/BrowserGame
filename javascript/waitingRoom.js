97da11b1ed7ecdb902a464374252ab6268fdd988
Balder J <baldertjacobsen@gmail.com>
2025-03-10 15:20:46 +0100

    add debug statement

--- javascript/waitingRoom.js
+++ javascript/waitingRoom.js
@@ -2,6 +2,7 @@
 const socket = io('https://130.225.170.52:10242'); // Use your actual server address and port
 
 socket.on('playerJoined', (data) => {
+    console.log('playerJoined event data:', data);
     // Check if the event is for the current game
     const currentGameId = document.getElementById('gameIdDisplay').textContent;
     if (data.gameId === currentGameId) {
